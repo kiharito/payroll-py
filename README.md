3bc60c52eb936c99992e55bf6000fdde1bac6bd9
kiharito <kiharito@users.noreply.github.com>
2025-06-08 18:48:59 +0900

    Run fastapi

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # payroll-py
+
+## 実行方法
+
+```shell
+uv run fastapi run main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from payroll-py!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def root():
+    return {"Hello": "World"}
